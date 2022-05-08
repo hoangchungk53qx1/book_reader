9542642d2e6ed1c73697985df16b82bd70503d60
HoangChung <hoangchungk53qx1@gmail.com>
2022-05-08 17:03:23 +0700

    update home done

--- lib/components/recent_card_builder.dart
+++ lib/components/recent_card_builder.dart
@@ -1,16 +1,14 @@
 import 'package:book_reader/components/loading_builder.dart';
 import 'package:book_reader/model/CategoryFeed.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 class RecentCardBuilder extends StatelessWidget {
   final Entry? entry;
 
-  RecentCardBuilder({Key? key, required this.entry
-  }) : super(key: key);
-  static final uuid = Uuid();
+  RecentCardBuilder({Key? key, required this.entry}) : super(key: key);
+  static const uuid = Uuid();
   final String imgTag = uuid.v4();
   final String titleTag = uuid.v4();
   final String authorTag = uuid.v4();
@@ -21,51 +19,87 @@ class RecentCardBuilder extends StatelessWidget {
   _buildBody(BuildContext context) {
     return InkWell(
       child: SizedBox(
-        height: 150.0,
         child: Row(
           children: [
             Card(
               shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))
-              ),
-              elevation: 10,
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              elevation: 20.0,
               child: ClipRRect(
                 borderRadius: const BorderRadius.all(Radius.circular(10.0)),
                 child: Hero(
                   tag: imgTag,
                   child: CachedNetworkImage(
                     imageUrl: '${entry?.link![1].href}',
-                    placeholder: (context, url) =>
-                    const LoadingBuilder(isLoadingPhoto: true,
+                    placeholder: (context, url) => const LoadingBuilder(
+                      isLoadingPhoto: true,
                     ),
-                    errorWidget: (context, url, error) =>
-                        Image.asset(
-                          'assets/images/place.png',
-                          fit: BoxFit.cover,
-                          height: 150.0,
-                          width: 150.0,),
+                    errorWidget: (context, url, error) => Image.asset(
+                      'assets/images/place.png',
+                      fit: BoxFit.cover,
+                      height: 150.0,
+                      width: 150.0,
+                    ),
                     fit: BoxFit.cover,
-                    width: 150.0,
+                    width: 120.0,
                     height: 150.0,
                   ),
                 ),
               ),
             ),
             const SizedBox(width: 20.0),
-            Flexible(child: Flexible(
+            Flexible(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Hero(tag: titleTag, child: Material(
-                    type: MaterialType.transparency,
-                    child: Text('${entry?.title?.t}', maxLines: 2,),
-                  ))
+                  Hero(
+                    tag: titleTag,
+                    child: Material(
+                      type: MaterialType.transparency,
+                      child: Text(
+                        '${entry?.title?.t}',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+                  Hero(
+                    tag: authorTag,
+                    child: Material(
+                      type: MaterialType.transparency,
+                      child: Text(
+                        '${entry?.author?.name?.t}',
+                        maxLines: 1,
+                        style: const TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue),
+                        textDirection: TextDirection.ltr,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    '${entry?.summary?.t}',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 12.0,
+                        color: Colors.black87),
+                  )
                 ],
               ),
-            ))
+            )
           ],
         ),
       ),
     );
   }
-
 }
